01137565e82972a2a9bc42ca6901e54fbb996137
nowkim <peterhyunjae@naver.com>
2021-07-25 18:58:26 +0900

    회원가입 및 이메일 인증시 올바르지 않은 이메일 형식일 때 return

--- app/main/service/mailer_service.py
+++ app/main/service/mailer_service.py
@@ -1,22 +1,22 @@
 from flask_mail import Message
 from main import mail
-from ..config import mailConfig
 
+# email 보내기
 def sendmail(data):
     # target email 주소
-    print(mail)
-    print(mailConfig)
-    
     client = data['email']
 
     msg = Message('Hello', sender = 'peter.hyunjae@gmail.com', recipients = [client])
+
+    # 랜덤 인증 코드 생성 / 인증 링크 생성 후 전송 - redirect로 인증하게 하는 방법 (이것도 만료 시간 해놓아야 될듯)
+    # 랜덤 인증 코드에 대해 세션 만료 시간 부여 (ex - 3분 안에 입력해야됨)
+    # 인증
+
     msg.body = "Hello Flask message sent from Flask-Mail"
 
     try:
-       mail.send(msg)
-       print('성공')
-       return "성공"
+        mail.send(msg)
+        return "성공"
     except Exception as e:
         print(e)
-        print('실패')
         return "실패"
--- app/main/service/user_service.py
+++ app/main/service/user_service.py
@@ -5,6 +5,8 @@ from main import db
 from main.model.user import User
 
 from ..service.mailer_service import sendmail
+# email 형식 틀린 것 걸러내기
+import re
 
 def generate_token(user):
     try:
@@ -25,27 +27,36 @@ def generate_token(user):
 
 #회원가입한 회원 정볼를 user모델(즉, user테이블에 넣기)
 def save_new_user(data):
-    user = User.query.filter_by(email=data['email']).first()
-    if not user:
-        new_user = User(
-            public_id=str(uuid.uuid4()),
-            email=data['email'],
-            username=data['username'],
-            password=data['password'],
-            registered_on=datetime.datetime.utcnow()
-        )
-        save_changes(new_user)
-        response_object = {
-            'status': 'success',
-            'message': '회원가입 되었습니다. '
-        }
-        return response_object, 201
+    # 맞는 email 형식인지 먼저 체크
+    if checkmail(data['email']):
+        user = User.query.filter_by(email=data['email']).first()
+        # db에 중복되는 email 주소 없음.
+        if not user:
+            new_user = User(
+                public_id=str(uuid.uuid4()),
+                email=data['email'],
+                username=data['username'],
+                password=data['password'],
+                registered_on=datetime.datetime.utcnow()
+            )
+            save_changes(new_user)
+            response_object = {
+                'status': 'success',
+                'message': '회원가입 되었습니다. '
+            }
+            return response_object, 201
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': '이미 가입된 email 주소입니다.',
+            }
+            return response_object, 409
     else:
         response_object = {
             'status': 'fail',
-            'message': '이미 가입된 email 주소입니다.',
+            'message': '입력한 email 주소는 맞는 형식이 아닙니다.'
         }
-        return response_object, 409
+        return response_object
 
 
 def get_all_users():
@@ -55,12 +66,27 @@ def get_a_user(id):
     return User.query.filter_by(id=id).first()
 
 def verify_a_user(data):
-    # 인증 메일을 보내고 난 후 인증 코드를 작성하고 post 한 후 상황
-    sendmail(data = data)
-    # data 와 실제 메일로 발송한 인증 코드 비교
-    # 맞을 경우 맞다고 return, 틀릴 경우 틀리다고 return.
-    return '메일 보냄!', 201
+    # 인증 메일 보내기 전 이메일 형식 확인
+    if checkmail(data['email']):
+        sendmail(data = data)
+        # 인증 메일을 보내고 난 후 인증 코드를 작성하고 post 한 후 상황
+        # data 와 실제 메일로 발송한 인증 코드 비교
+        # 맞을 경우 맞다고 return, 틀릴 경우 틀리다고 return.
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': '입력한 email 주소는 맞는 형식이 아닙니다.'
+        }
+        return response_object
 
 def save_changes(data):
     db.session.add(data)
     db.session.commit()
+
+# email 형식 체크
+def checkmail(email):
+    p = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+    result = p.match(email) != None
+    
+    #True, False로 return
+    return result
