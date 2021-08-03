f53a5e2ebb967da6c52d9f7fa025e4d0e727909f
Seungwoo Kim <sonicdx886@gmail.com>
2021-08-03 21:13:56 +0900

    UserSubject model modified

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ from flask_script import Manager
 
 from app import blueprint
 from app.main import create_app, db
-from app.main.model import user #model을 정의한 폴더 import
+from app.main.model import user, user_subject #model을 정의한 폴더 import
 from app.main.model.subjects import t_departments, t_s20_1, t_s20_2, t_s20_s, t_s20_w, t_s21_1, t_s21_2, t_s21_s
 
 # 환경 변수에서 필요한 매개 변수를 사용하여 응용 프로그램 인스턴스를 생성 초기에 만들어진 기능 - dev, prod, test. 환경 변수에 아무것도 설정되지 않은 경우 기본값 dev이 사용됩니다.
