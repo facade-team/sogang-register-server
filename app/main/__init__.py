1966498b5eacea11c816447c2b9a5463d7c38fb8
Seungwoo Kim <sonicdx886@gmail.com>
2021-08-15 20:12:31 +0900

    cors url 셋팅 변경

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -25,8 +25,8 @@ def create_app(config_name):
     db.init_app(app)
     flask_bcrypt.init_app(app)
     #CORS(app)
-    #CORS(app, resources={r'*': {'origins': 'http://localhost:3000'}})
-    CORS(app, resources={r'*': {'origins': '*'}})
+    CORS(app, resources={r'*': {'origins': 'http://sogang-sincheong.com/'}})
+    #CORS(app, resources={r'*': {'origins': '*'}})
 
 
     #mail 관련 config - main/config에 추가, mount
